a550562e385bfbeb1f519b44e64b89006cd5fb7b
Priyank davda <priyank.davda@sinelogix.com>
2023-02-13 15:55:04 +0530

    Create cart table and migrate file

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -37,7 +37,7 @@
           <button type="submit" class="btn btn-default">Search</button>
         </form>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Add to Cart</a></li>
+          <li><a href="#">Cart</a></li>
           {{-- <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
             <ul class="dropdown-menu">

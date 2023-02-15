4f7cff6df12b63ff10ca8552b04f7ee616576b2a
Priyank davda <priyank.davda@sinelogix.com>
2023-02-15 10:06:41 +0530

    add to product to cart. create a cart model and login is not than add to cart button on click than redirect the login page and login is already than product add to cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
 
 class ProductController extends Controller
 {
@@ -24,5 +25,19 @@ class ProductController extends Controller
         //return $req->input();
         $data=Product::where('name', 'like', '%' .$req->input('query').'%')->get();
         return view('search',['products'=>$data]);
+    }
+    function addToCart(Request $req)
+    {
+        //return "hello";
+        if($req->session()->has('user'))
+        {
+            $cart=new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+        }else{
+            return redirect('/login');
+        }
     }
 }
